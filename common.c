d4a31287c12dc11bb1c9774e0d54ea4cdfe64be8
Your Name <you@example.com>
2025-10-27 16:06:48 +0800

    clib

--- common.c
+++ common.c
@@ -2,6 +2,43 @@
 
 void putchar(char ch);
 
+// 修改記憶體內容
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+// 複製記憶體內容
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+// 比較字串內容
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+// 比較字串大小
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
 void printf(const char *fmt, ...) {
     va_list vargs;
     va_start(vargs, fmt);
